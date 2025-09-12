ba83da8d164e5e2fd43473a514a6292c80d0a67d
Dylan Cygul <dylan.cygul@gmail.com>
2025-09-12 15:30:01 -0400

    added fruit merge minigame and study timer

--- Scripts/start_button.gd
+++ Scripts/start_button.gd
@@ -1,18 +1,10 @@
 extends Button
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+# when start button pressed
 func _on_pressed() -> void:
 	var tween = create_tween()
 	var target_position = Vector2(-550, 0)
 	
 	tween.tween_property($"../..", "position", target_position, 0.7).set_trans(Tween.TRANS_QUART)
+	
