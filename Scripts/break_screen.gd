87adc1dfacc523623e47e5ab496246ec248db8b7
Dylan Cygul <dylan.cygul@gmail.com>
2025-09-15 19:27:26 -0400

    added random minigame folder access

--- Scripts/break_screen.gd
+++ Scripts/break_screen.gd
@@ -3,9 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# TODO: Generate random minigame not just this one
-	var minigameObj = preload("res://Minigames/Fruit Merge/Scenes/fruit_merge_game.tscn")
+	# Generates random minigame from minigame folder
+	
+	var file_names : Array = DirAccess.get_directories_at("res://Minigames")
+	var random_minigame : String = file_names.pick_random()
+	var path : String = "res://Minigames/" + random_minigame + "/Scenes/minigame_scene.tscn"
+	
+	var minigameObj := load(path)
 	
 	var minigame = minigameObj.instantiate()
-	
 	add_child(minigame)
