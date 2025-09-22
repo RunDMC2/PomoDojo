caa9fb61710747ace336c2f9bb7fe1f6881fc8f6
RunDMC2 <dylan.cygul@gmail.com>
2025-09-22 19:11:44 -0400

    issue #2 solved

--- Minigames/Fruit Merge/Scripts/fruit_merge_game.gd
+++ Minigames/Fruit Merge/Scripts/fruit_merge_game.gd
@@ -139,21 +139,12 @@ func _on_death_plane_entered(body: Node2D) -> void:
 	move_tween.tween_property($"Win or Loss Screen/Win Screen", "position:x", 0, 1).set_ease(Tween.EASE_IN_OUT)
 """
 	
+	
+# THIS LOSE FUNCTION MUST BE IN EVERY MINIGAME
 func lose():
 	game_over = true
-	var opac_tween = create_tween()
-	opac_tween.tween_property($"Game Over Screen/Background", "modulate:a", 0.75, 0.5).set_ease(Tween.EASE_IN_OUT)
 	
+	var end_screen_Obj = load("res://Minigames/end_screen.tscn")
+	var end_screen = end_screen_Obj.instantiate()
+	add_child(end_screen)
 	
-	await get_tree().create_timer(1).timeout 
-
-	var move_tween = create_tween()
-	move_tween.tween_property($"Game Over Screen/Loss Screen", "position:x", 0, 1).set_ease(Tween.EASE_IN_OUT)
-		
-
-func _on_exit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_manu.tscn")
-
-
-func _on_keep_studying_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/study_screen.tscn")
